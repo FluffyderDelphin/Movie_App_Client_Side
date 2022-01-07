ece596aa6f78352c5fcd1c14ffd04a2c1158f575
Alexander Urlich <ulrichalex12@yahoo.de>
2022-01-07 21:56:16 +0100

    linked Movie List From MovieApp Api

--- src/components/main-view/main-view.js
+++ src/components/main-view/main-view.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,28 +7,21 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Inception',
-          Description: 'desc1...',
-          ImagePath: '...',
-        },
-        {
-          _id: 2,
-          Title: 'The Shawshank Redemption',
-          Description: 'desc2...',
-          ImagePath: '...',
-        },
-        {
-          _id: 3,
-          Title: 'Gladiator',
-          Description: 'desc3...',
-          ImagePath: '...',
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
+  }
+  componentDidMount() {
+    axios
+      .get('https://alexandersmovieapp.herokuapp.com/movies')
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -36,9 +30,8 @@ export class MainView extends React.Component {
   }
   render() {
     const { movies, selectedMovie } = this.state;
-
-    if (movies.length === 0)
-      return <div className="main-view">The list is empty!</div>;
+    console.log(movies);
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
