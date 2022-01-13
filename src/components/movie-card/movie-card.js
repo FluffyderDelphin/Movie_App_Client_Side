68fe36f0a619f8887cf72fa66569b5c8f115fc79
Alexander Urlich <ulrichalex12@yahoo.de>
2022-01-13 01:50:39 +0100

    Movie-Card Styling

--- src/components/movie-card/movie-card.js
+++ src/components/movie-card/movie-card.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
     return (
-      <div
-        className="movie-card"
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col xs={8}>
+            <Card>
+              <Card.Img variant="top" src={movie.imageurl} />
+              <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.description}</Card.Text>
+                <Button onClick={() => onMovieClick(movie)} variant="link">
+                  Open
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     );
   }
 }
